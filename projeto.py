fe4aba0d2131ef72152825fb48276f5d24da68bc
analununes <53786425+analununes@users.noreply.github.com>
2019-09-05 18:12:54 -0300

    adicionando à classe pessoa

--- projeto.py
+++ projeto.py
@@ -1,7 +1,8 @@
 import numpy as np
-
+from random import random
 
 class Pessoa:
+    
     def __init__(self):
         self.sexo       = None
         self.cor        = None
@@ -9,10 +10,41 @@ class Pessoa:
         self.modalidade = None
         self.ocupacao   = None
 
+        
     def pessoa(self):
-        pass
+           if random() < np.random.uniform(0.428-0.0095, 0.428+0.0095):
+            self.sexo = "mulher"
+        else: 
+            self.sexo = "homem"
+            
+            
+        if random() < np.random.uniform(0.421-0.012, 0.421+0.012):
+            self.cor = "preta_parda"
+        else: 
+            self.cor = "branca"
+            
+            
+        if random() < np.random.uniform(0.22-0.052, 0.22+0.052):
+            self.rede = "publica"
+        else:
+            self.rede = "particular" 
+            
+            
+        if random() < np.random.uniform(0.178-0.051, 0.178+0.051):
+            self.modalidade = "distancia"
+        else:
+            self.modalidade = "presencial"
 
+
+        if random() < np.random.uniform(0.346-0.011,0.346+0.011):
+            self.ocupacao = "nao_ocupado"
+        else:
+            self.ocupacao = "ocupado"
+            
+            
+            
 class Populacao:
+    
     def __init__(self, N=1000):
         self.pop = N
         self.pessoas = []
@@ -23,4 +55,4 @@ class Populacao:
             self.pessoas.append(p)
 
     def amostra(self, n = 100):
-        np.random.choice(self.pessoas, n)
+        return np.random.choice(self.pessoas, n)
