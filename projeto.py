cf4edb278f01fdf8b5f3fe4275c278c3790e7129
Yolanda Davila Lins Nogueira <B39378@fgv.edu.br>
2019-09-05 14:36:09 -0300

    classe pessoa

---
+++ projeto.py
@@ -0,0 +1,17 @@
+import numpy as np
+from random import random
+
+
+class Pessoa:
+    def __init__(self, parametros):
+        self.parametros = parametros
+
+
+''' 
+parametros é uma lista com os atributos do individuo: 
+elemento_1 = sexo 
+elemento_2 = cor 
+elemento_3 = salario 
+elemento_4 = rede de ensino 
+elemento_5 = modalidade de ensino 
+'''
