254d6ddb2726cc275e4b2f611a58396811061c8a
Ana Luiza Nunes <B39430@fgv.edu.br>
2019-09-05 14:39:21 -0300

    rascunho da classe população

--- projeto.py
+++ projeto.py
@@ -15,3 +15,17 @@ elemento_3 = salario
 elemento_4 = rede de ensino 
 elemento_5 = modalidade de ensino 
 '''
+
+
+class Populacao:
+    def __init__(self, N=1000, n=200):
+        self.pop = N
+        self.amostra = n
+        self.pessoas = []
+
+        for i in range(0, N):
+
+        random.random()
+
+    def amostra(self, n):
+        np.random.choice(self.pessoas, n)
